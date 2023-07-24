31c1bbc9e47162b44afbd09aee78dffc245ae43c
Paulo301 <paulovictor1918@gmail.com>
2023-07-24 12:59:56 -0300

    feat: create button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,55 @@
+import { primary, white } from '@/styles/colors';
+import MUIButton, { ButtonProps as MUIButtonProps } from '@mui/material/Button';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { ReactNode } from 'react';
+
+interface ButtonProps extends MUIButtonProps {
+  children: ReactNode;
+}
+
+const customTheme = createTheme({
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: ({ ownerState }) => ({
+          ...(ownerState.size === 'small'  && {
+              fontSize: '0.875rem',
+              lineHeight: '1rem',
+              padding: '0.5rem 1rem',
+              gap: '0.5rem',
+            }),
+          ...(ownerState.size === 'medium'  && {
+            fontSize: '0.875rem',
+            lineHeight: '1rem',
+            padding: '0.75rem 1rem',
+            gap: '0.5rem',
+          }),
+          ...(ownerState.size === 'large'  && {
+            fontSize: '1rem',
+            lineHeight: '1.5rem',
+            padding: '0.875rem 1.5rem',
+            gap: '0.625rem',
+          }),
+          fontFamily: 'Roboto, sans-serif',
+          borderRadius: '32px',
+          backgroundColor: primary[500],
+          color: white,
+          textTransform: 'capitalize',
+          ':hover': {
+            backgroundColor: primary[600],
+          }
+        }),
+      },
+    }
+  }
+});
+
+export function Button({ children, ...rest }: ButtonProps) {
+  return (
+    <ThemeProvider theme={customTheme}>
+      <MUIButton {...rest}>
+        {children}
+      </MUIButton>
+    </ThemeProvider>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { FormControlLabel, Typography } from "@mui/material";
+import { Button } from "@/components/Button";
+import { Typography } from "@mui/material";
 
 export default function Home() {
   return (
@@ -6,6 +7,9 @@ export default function Home() {
       <Typography variant="h1">
         Hello World
       </Typography>
+      <Button size="medium">
+        Teste
+      </Button>
     </>
     
   )
